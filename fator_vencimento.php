818f3174841441140d5b2da914ba7987d7ba1224
VLADEMIR <30865894+vladetec@users.noreply.github.com>
2025-02-19 12:19:47 -0300

    Update fator_vencimento.php

--- fator_vencimento.php
+++ fator_vencimento.php
@@ -1,15 +1,16 @@
 <?php
+date_default_timezone_set('America/Sao_Paulo');
 
 define('DATA_BASE_ANTIGA', strtotime('1997-10-07'));
 define('DATA_LIMITE', strtotime('2025-02-22'));
-define('DATA_BASE_NOVA', DATA_LIMITE - (1000 * 86400)); // 1000 dias antes de DATA_LIMITE
+define('DATA_BASE_NOVA', DATA_LIMITE - 1000 * 24 * 60 * 60);
 
 function calcular_fator_antigo($data_vencimento) {
-    return ($data_vencimento - DATA_BASE_ANTIGA) / 86400;
+    return ($data_vencimento - DATA_BASE_ANTIGA) / (60 * 60 * 24);
 }
 
 function calcular_fator_novo($data_vencimento) {
-    return ($data_vencimento - DATA_BASE_NOVA) / 86400;
+    return ($data_vencimento - DATA_BASE_NOVA) / (60 * 60 * 24);
 }
 
 function calcular_fator_vencimento($data_vencimento) {
@@ -21,3 +22,10 @@ function calcular_fator_vencimento($data_vencimento) {
 }
 
 echo "Digite a data de vencimento (dd/mm/yyyy): ";
+$vencimento_str = trim(fgets(STDIN));
+$data_vencimento = strtotime(str_replace("/", "-", $vencimento_str));
+$fator_vencimento = calcular_fator_vencimento($data_vencimento);
+
+echo "Data de vencimento: " . date('d/m/Y', $data_vencimento) . "\n";
+echo "Fator de vencimento: " . $fator_vencimento . "\n";
+
